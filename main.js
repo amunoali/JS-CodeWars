da40158b56c06ce56f69b2b3e0f90d98217a3117
amunoali <amunoali@gmail.com>
2022-04-18 14:00:00 -0400

    initial

--- main.js
+++ main.js
@@ -125,12 +125,21 @@
 // }
 
 //same case porblem
-function sameCase(a, b){
-  if(a.toUpperCase() === a.toLowerCase() && b.toLowerCase() === b.toUpperCase()) {
-     return -1
-     }else if(a === a.toLowerCase() && b === b.toLowerCase() || b === b.toUpperCase() && a == a.toUpperCase){
-       return 1
-     }else{
-       return 0
-     }
-}
+// function sameCase(a, b){
+//   if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+//      return -1
+//    }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+//        return 1
+//    }else{
+//      return 0
+//    }
+//  }
+
+//  function sameCase(a, b){
+//   // Am regex fiend, but trying to force myself not to use it
+//   // If a character is not a letter then the cases will match each other
+//   if (a.toLowerCase() === a.toUpperCase() || b.toLowerCase() === b.toUpperCase()) {
+//     return -1;
+//   }
+//   return (a.toLowerCase() === a) === (b.toLowerCase() === b) ? 1 : 0;
+// }
