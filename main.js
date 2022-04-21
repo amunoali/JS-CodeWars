ab2ed20b46fff418b6237cda2e06a3b496a7b0ef
amunoali <amunoali@gmail.com>
2022-04-20 23:14:35 -0400

    new challenge

--- main.js
+++ main.js
@@ -19,22 +19,35 @@
 // function smallValue(s){
 //     return Math.min(...s.split(" ").map (s => s.length));
 // }
-function solution(number){
-    let sum = 0
-    for (let i = 1; i < number.length; i++){
-      if(number % 3 === 0 || number % 5 === 0){
-        sum +=1
-      }
-        return sum
-      }
-  }
-  function solution(number){
-    var sum = 0;
+// function solution(number){
+//     let sum = 0
+//     for (let i = 1; i < number.length; i++){
+//       if(number % 3 === 0 || number % 5 === 0){
+//         sum +=1
+//       }
+//         return sum
+//       }
+//   }
+//   function solution(number){
+//     var sum = 0;
     
-    for(var i = 1;i< number; i++){
-      if(i % 3 == 0 || i % 5 == 0){
-        sum += i
-      }
-    }
-    return sum;
-  }
+//     for(var i = 1;i< number; i++){
+//       if(i % 3 == 0 || i % 5 == 0){
+//         sum += i
+//       }
+//     }
+//     return sum;
+//   }
+
+// function inArray(array1,array2){
+//   return array1
+//     .filter(a1 => array2.find(a2 => a2.match(a1)))
+//     .sort()
+// }
+
+//return odd or even
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
