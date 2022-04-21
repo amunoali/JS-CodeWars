75ebca02e1a3de2bd8eb41dc7912e1a669c58a65
amunoali <amunoali@gmail.com>
2022-04-20 21:04:34 -0400

    new code challenge

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-//gravity flip
+// // //gravity flip
 
-const flip=(d, a)=>{
-    if(d === 'R') return a.sort((a,b)=>a-b);
-    if(d === 'L') return a.sort((a,b)=>b-a);
-  }
+// // const flip=(d, a)=>{
+// //     if(d === 'R') return a.sort((a,b)=>a-b);
+// //     if(d === 'L') return a.sort((a,b)=>b-a);
+// //   }
 
-  const flip = (d, a) => d === 'R' ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a)
+// //   const flip = (d, a) => d === 'R' ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a)
 
-  const flip = (d, a) => d === "R" ? a.sort((a, b) => a - b) : a.sort((a, b) => b-a)
+// //   const flip = (d, a) => d === "R" ? a.sort((a, b) => a - b) : a.sort((a, b) => b-a)
+
+// // highAndLow("1 2 3 4 5");  // return "5 1"
+
+// function highAndLow(numbers){
+//     numbers = numbers.split(' ')
+//     return `${Math.max(...numbers)} ${Math.min(...numbers)}`
+// }
