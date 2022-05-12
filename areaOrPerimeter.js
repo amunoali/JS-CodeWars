f4c35516310daff870a7226d4f7e02695b9ce396
Muno Ali <97273058+amunoali@users.noreply.github.com>
2022-05-12 10:00:48 -0400

    Update areaOrPerimeter.js

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -1,5 +1,5 @@
 // You are given the length and width of a 4-sided polygon. The polygon can be a rectangle or a square.
-// If it is a square, return its area. If it is a rectangle, return its perimeter.
+// If it's square, return its area. If it's rectangle, return its perimeter.
 
 // area_or_perimeter(6, 10) --> 32
 // area_or_perimeter(3, 3) --> 9
