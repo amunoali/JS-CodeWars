c7339f72fde8b6df687e827755f467a92e116310
Muno Ali <97273058+amunoali@users.noreply.github.com>
2022-05-16 01:37:32 -0400

    Add files via upload

---
+++ toSquareOrNot.js
@@ -0,0 +1,21 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+
+function squareOrSquareRoot(array){
+    return array.map(x =>{
+        const num = Math.sqrt(x)
+        return (num % 1 == 0) ? num : (x*x)
+    })
+}
+
+// best practice:
+// const squareOrSquareRoot = array => array.map(a => Math.sqrt(a) % 1 == 0 ? Math.sqrt(a) : (a * a));
