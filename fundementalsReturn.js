dc96f136383891e02f40a7db0a00945c8f27b2af
Muno Ali <97273058+amunoali@users.noreply.github.com>
2022-05-22 11:54:31 -0400

    Update fundementalsReturn.js

--- fundementalsReturn.js
+++ fundementalsReturn.js
@@ -41,3 +41,12 @@ function exponent(a,b){
 function subt(a,b){
     return a -b 
 }
+
+
+best practice:
+// const add = (a, b) => a + b;
+// const subt = (a, b) => a - b;
+// const divide = (a, b) => a / b;
+// const multiply = (a, b) => a * b;
+// const mod = (a, b) => a % b;
+// const exponent = (a, b) => a ** b;
