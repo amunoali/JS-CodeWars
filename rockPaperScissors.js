8b052782c4f2d53a5e0ee5fea3f3525ab1ea2e4f
Muno Ali <97273058+amunoali@users.noreply.github.com>
2022-05-26 22:13:38 -0400

    Add files via upload

---
+++ rockPaperScissors.js
@@ -0,0 +1,43 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+    if (p1 == p2)
+      return 'Draw!';
+      
+     if (p1 == 'rock' && p2 == 'scissors') 
+       return 'Player 1 won!'
+     else if (p1 == 'scissors' && p2 == 'paper') 
+       return 'Player 1 won!'
+     else if (p1 == 'paper' && p2 == 'rock') 
+       return 'Player 1 won!'
+     else
+       return 'Player 2 won!';
+  };
+//   other:
+
+// const rps = (p1, p2) => {
+//     if (p1 === p2) return 'Draw!';
+//     if (p1 === 'rock' && p2 === 'scissors') return 'Player 1 won!';
+//     if (p1 === 'scissors' && p2 === 'paper') return 'Player 1 won!';
+//     if (p1 === 'paper' && p2 === 'rock') return 'Player 1 won!';
+//     return 'Player 2 won!';
+//   };
+
+//   other options:
+//   const rps = (p1, p2) => {
+//     if (p1 === p2) return "Draw!";
+//     var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//     if (p2 === rules[p1]) {
+//       return "Player 1 won!";
+//     }
+//     else {
+//       return "Player 2 won!";
+//     }
+//   };
+
