e6004347bd09cc6b0720eec99ffda58e57ba57bf
amunoali <amunoali@gmail.com>
2022-08-16 13:42:26 -0400

    codewars of the day

---
+++ executeMeNTimes.js
@@ -0,0 +1,15 @@
+// You're given a procedure / method that executes a passed action n times, but it does so in a purely sequential manner. If you inspect the sample tests, you'll find that the action takes about 1 second to complete, and this action is repeated 20 times which results in a timeout.
+
+// Find a way to execute the action n times in parallel, such that the sample tests complete under the 12 second time limit imposed by Codewars. The submission tests will also test for similar scenarios.
+
+// Hint: If you're not sure where to start, read up on async/await.
+
+
+function execute(action, nTimes){
+    let result = []
+    for(let i = 0; i < nTimes; i++){
+      result.push(action())
+    }
+   return Promise.all(result)
+  }
+    
