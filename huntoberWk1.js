62cfa584653cef07e1fb1b139f95d6895ab768d4
amunoali <amunoali@gmail.com>
2022-10-02 17:30:43 -0400

    codewar of the day

---
+++ huntoberWk1.js
@@ -0,0 +1,28 @@
+// This week we will be discovering hidden messages by manipulating a given string. Your solutions should account for any non-empty string. On Friday we'll combine our functions to decode a message.
+
+// Oh no! How did this happen?! Somehow similar-looking numbers and letters have been confused in our document. Someone may have been messing around writing naughty words on a calculator. We need to get them back to normal.
+
+// Below, find the mix-ups that have been made. Note: the confusion happened in both directions and was case-sensitive.
+
+
+// COPY
+// '0' <-> 'O'      '5' <-> 'S'
+// '1' <-> 'I'      '6' <-> 'G'
+// '2' <-> 'Z'      '7' <-> 'L'
+// '3' <-> 'E'      '8' <-> 'B'
+// '4' <-> 'h'      '9' <-> 'q'
+
+// "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
+// // implement fix function -> 
+// "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+
+//Prep
+//P:taking in numbers and letters 
+//r:decoding for the numbers to switch to letters so that it can spell out the word
+//e:"PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770."  ->  "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+//p: I can hardcode and write each letter associated with the proper number. 
+
+
+function getThatMessage(letter){
+    
+}
