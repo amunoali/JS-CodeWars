f38cebec6d665b575b8740a0a995a58f99793b53
amunoali <amunoali@gmail.com>
2022-10-18 18:25:23 -0400

    prep techn

---
+++ huntober3.js
@@ -0,0 +1,21 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+// assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+// assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+//taking array of strings and numbers-any neg
+//returning sum of the numbers
+//arrays come with methods 
+
+
+function sumMix(arr){
+    // return arr.reduce((acc, c)=> acc + Number(c), 0)
+    return arr.reduce((acc, c)=> acc + +c, 0)
+}
+
+console.log(sumMix([9, 3, '7', '3']))
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']))
