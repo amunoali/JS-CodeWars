120d3804c1fbbb1db9f3f1f627bdc3d594692d84
amunoali <amunoali@gmail.com>
2022-10-24 18:24:27 -0400

    huntober

--- huntober6.js
+++ huntober6.js
@@ -9,11 +9,14 @@
 //taking in an array and number
 //returnring array last even number
 
-function allEven(arr, num){
-    //can map to get all even number
-return arr.filter(x=> x % 2 == 0).slice(-num)
-}
+// function allEven(arr, num){
+//     //can map to get all even number
+// return arr.filter(x=> x % 2 == 0).slice(-num)
+// }
+
+const allEven = (arr, num) => arr.filter(x=> x % 2 == 0).slice(-num)
 
 console.log(allEven([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 console.log(allEven([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
 console.log(allEven([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
+
