e88e7c93cea347652eda276d81079bc2dd3430cb
amunoali <amunoali@gmail.com>
2022-11-30 18:21:20 -0500

    huntober

--- huntober19.js
+++ huntober19.js
@@ -11,15 +11,22 @@
 // "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
 // "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
 
+// function missingVowel(str){
+// //list all vowels - loop through each vowel  and see if str exist 
+// let vowels = 'aeiou'
+// for (let i = 0; i< vowels.length; i++){
+//     if(str.indexOf(vowels[i]) == -1) //whatever vowel im currenly in if theres an index of a, e in the string. if letter not inside str, it wont have index. index also gives -1 val, loop through each val and see if a has an index inside str
+//     return i
+// }
+
+// }
+
+// //loop inside loop- its O(5n)
+
 function missingVowel(str){
-//list all vowels - loop through each vowel  and see if str exist 
-let vowels = 'aeiou'
-for (let i = 0; i< vowels.length; i++){
-    if(str.indexOf(vowels[i]) == -1) //whatever vowel im currenly in if theres an index of a, e in the string. if letter not inside str, it wont have index. index also gives -1 val, loop through each val and see if a has an index inside str
-    return i
-}
+    return [...'aeiou'].findIndex(x => !str.includes(x))
+
 
 }
-
 console.log(missingVowel("John Doe hs seven red pples under his bsket"  ), '0' )
 console.log(missingVowel("Bb Smith sent us six neatly arranged range bicycles"  ), '3' )
