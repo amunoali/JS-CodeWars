f4ba8ebc1f370d85ca7a507a0b3b9d7bef5cfb90
Muno Ali <amunoali@gmail.com>
2023-01-29 11:04:54 -0500

    codewars

--- js/huntober17.js
+++ js/huntober17.js
@@ -5,9 +5,9 @@
 // --> "alpha beta gamma delta alpha beta gamma delta"
 //getting string of words, returning a string that doesnt have duplicates
 
-function removeDupeWords(str){
-//str - split '  ' => filter conscecutive duplicates- if this word is the same as the word before, dont include it. filter take sin 3 pramters-el (current word), index(next), arr
-return str.split(' ').filter((e, index, arr)=> e !== arr[index -1]).join(' ')
-}
+// function removeDupeWords(str){
+// //str - split '  ' => filter conscecutive duplicates- if this word is the same as the word before, dont include it. filter take sin 3 pramters-el (current word), index(next), arr
+// return str.split(' ').filter((e, index, arr)=> e !== arr[index -1]).join(' ')
+// }
 
-console.log(removeDupeWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"), "alpha beta gamma delta alpha beta gamma delta")
+// console.log(removeDupeWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"), "alpha beta gamma delta alpha beta gamma delta")
