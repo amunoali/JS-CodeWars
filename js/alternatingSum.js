6a815b740ed3632859dc7b00265f5554896579f8
Muno Ali <amunoali@gmail.com>
2023-03-06 12:36:51 -0500

    codesignal

--- js/alternatingSum.js
+++ js/alternatingSum.js
@@ -9,7 +9,7 @@
 
 
 function solution(a) {
-    return [a.filter((el,i)=>i%2===0).reduce((b,c)=>b+c,0), a.filter((el,i)=>i%2!==0).reduce((b,c)=>b+c,0)];
+    return [a.filter((e,i)=>i%2===0).reduce((b,c)=>b+c,0), a.filter((e,i)=>i%2!==0).reduce((b,c)=>b+c,0)];
 
 }
 
